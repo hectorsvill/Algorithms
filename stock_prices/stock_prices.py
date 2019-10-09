ffa203960095434df6e814aa5bf149b2ea9efefc
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-09 16:25:25 -0700

    test failed

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,19 +4,28 @@ import argparse
 
 def find_max_profit(prices):
     print("here")
+
+    prices_len = len(prices)
     i = 0
-    current_min_price_so_far = prices[0]
+    current_min_price_so_far = 0
     find_max_profit = 0
 
-    while (i < len(prices)):
-        print(prices[i])
+    while (i < prices_len):
+
+        p = 0
+        while(p < prices_len):
+            # print(abs(prices[i] - prices[p]))
+            if current_min_price_so_far < abs(prices[i] - prices[p]):
+                current_min_price_so_far = abs(prices[i] - prices[p])
+            p += 1
+
         i += 1
 
-    return -1
+    return current_min_price_so_far
 
-l = [10, 7, 5, 8, 11, 9]
-find_max_profit(l)
-
+# l = [10, 7, 5, 8, 11, 9]
+# x = find_max_profit(l)
+# print(x)
 
 #
 # if __name__ == '__main__':
