18f89ef83eeaa758f926cbbe566d341d23a1e89c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-10 08:52:42 -0700

    recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,39 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  maximum_number_of_whole_batches = 0
+  for (key, value) in ingredients.items():
+    ingredients[key] -= recipe[key]
+    print(f"{key} - {value}\n{recipe[key]}")
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  for (key, value) in ingredients.items():
+      if value < 0:
+        print(f"{ingredients}")
+        return maximum_number_of_whole_batches
+      else:
+          print(f"{key} - {value}\n{recipe[key]}")
+          return recipe_batches(recipe, ingredients)
+  
+  
+  print(f"{ingredients}")
+  return 0
+  # while 1:
+
+
+
+
+
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+
+
+
+
+recipe_batches({ 'milk': 100, 'butter': 50, 'flour': 5 }, 
+                { 'milk': 138, 'butter': 48, 'flour': 51 })
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,29 +3,28 @@
 import argparse
 
 def find_max_profit(prices):
-    print("here")
-
     prices_len = len(prices)
-    i = 0
     current_min_price_so_far = 0
     find_max_profit = 0
 
+    i = 0
     while (i < prices_len):
 
         p = 0
         while(p < prices_len):
             # print(abs(prices[i] - prices[p]))
-            if current_min_price_so_far < abs(prices[i] - prices[p]):
-                current_min_price_so_far = abs(prices[i] - prices[p])
+            if current_min_price_so_far < prices[i] - prices[p]:
+                current_min_price_so_far = prices[i] - prices[p]
             p += 1
 
         i += 1
 
     return current_min_price_so_far
 
-# l = [10, 7, 5, 8, 11, 9]
-# x = find_max_profit(l)
-# print(x)
+l1 = [100, 90, 80, 50, 20, 10] # -10
+l2 = [10, 7, 5, 8, 11, 9] # 6
+x = find_max_profit(l2)
+print(x)
 
 #
 # if __name__ == '__main__':
